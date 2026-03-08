2eb0fd7b93ba166b013a08fff46675afb8dffcf0
Kavisha <11203643@students.cs.ubc.ca>
2026-03-07 16:15:30 -0800

    added gitignore

--- .gitignore/config.js
+++ .gitignore/config.js
@@ -1,5 +1,5 @@
 // config.js
 export const CONFIG = {
-    GEMINI_API_KEY: "your_actual_key_here", // Get from Google AI Studio
+    GEMINI_API_KEY: "AIzaSyAo8ZiIh9zhnltGzckhAFQ3RLkr5QxIO6A", // Get from Google AI Studio
     API_URL: "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent"
 };
--- popup.js
+++ popup.js
@@ -1,4 +1,4 @@
-import { CONFIG } from './config.js';
+import { CONFIG } from './.gitignore/config.js';
 
 /**
  * PERSON 3: Fetch Socratic Explanation
